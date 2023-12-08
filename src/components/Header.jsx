75095455233644e4b9787c1e8abf8fd435d2a792
Shahid Rashid Bhat <sr14201721@gmail.com>
2023-12-08 15:33:30 +0530

    initail commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BookmarkAddIcon from '@mui/icons-material/BookmarkAdd';
+
+function Header() {
+  return (
+    <header>
+      <h1><BookmarkAddIcon/>Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
